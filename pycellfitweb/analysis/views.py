6999b8e66d10542e44161ab37aa5e7ed1c50dc87
Nilai Vemula <nilaivemula@gmail.com>
2020-07-07 14:44:25 -0500

    make plotly load faster and remove logo

--- pycellfitweb/analysis/views.py
+++ pycellfitweb/analysis/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect
 from .forms import AnalysisForm
 import base64
 from PIL import Image
+import numpy as np
 
 
 # default home page view
@@ -19,7 +20,6 @@ def about(request):
 def analysis(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
-        print('post')
         # create a form instance and populate it with data from the request:
         form = AnalysisForm(request.POST, request.FILES)
         
@@ -28,29 +28,35 @@ def analysis(request):
             print('form is valid')
             print(form.cleaned_data)
         
-        # extract input parameters from form
-        name = form.cleaned_data['name']
-        image = form.cleaned_data['image']
+            # extract input parameters from form
+            name = form.cleaned_data['name']
+            image = form.cleaned_data['image']
 
 
-        # convert uploaded image to np array
-        im = Image.open(image)
-        img_array = np.array(im)
+            # convert uploaded image to np array
+            im = Image.open(image)
+            img_array = np.array(im)
 
-        # plot np array using plotly
-        import plotly.express as px
-        from plotly.offline import plot
-    
-        fig = px.imshow(img_array)
-        plt_div = plot(fig, output_type='div')
+            # plot np array using plotly
+            import plotly.express as px
+            from plotly.offline import plot
+        
+            fig = px.imshow(img_array)
+            config = {
+                'scrollZoom': False,
+                'displayModeBar': True,
+                'showLink':False,
+                'displaylogo': False
+            }
+            plt_div = plot(fig, output_type='div', include_plotlyjs=False, show_link=False, link_text="", config=config)
 
-        # send all form, information, and plot to html template
-        context = {
-            'name': name,
-            'form': form,
-            'plot': plt_div
-        }
-        return render(request, 'analysis/output.html', context)
+            # send all form, information, and plot to html template
+            context = {
+                'name': name,
+                'form': form,
+                'plot': plt_div
+            }
+            return render(request, 'analysis/output.html', context)
 
     # if a GET (or any other method) we'll create a blank form
     else:
