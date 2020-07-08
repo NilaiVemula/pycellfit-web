97163f969c9f268f18c96a0f855895a9bdad33ea
Nilai Vemula <nilaivemula@gmail.com>
2020-07-08 00:45:52 -0500

    added bootstrap themes

--- pycellfitweb/analysis/views.py
+++ pycellfitweb/analysis/views.py
@@ -10,7 +10,7 @@ from io import BytesIO
 
 # default home page view
 def home(response):
-    return render(response, 'analysis/home.html')
+    return render(response, 'analysis/home_alt.html')
 
 # view for about page
 
@@ -106,4 +106,4 @@ def analysis(request):
             'form': form
         }
 
-        return render(request, 'analysis/form.html', context)
+        return render(request, 'analysis/upload_form_alt.html', context)
