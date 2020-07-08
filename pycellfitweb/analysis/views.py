80eb9e3ab8882040a25c1695d55da1b9448f40e1
Nilai Vemula <nilaivemula@gmail.com>
2020-07-08 01:30:49 -0500

    reformat file

--- pycellfitweb/analysis/views.py
+++ pycellfitweb/analysis/views.py
@@ -9,6 +9,8 @@ import numpy as np
 from io import BytesIO
 
 # default home page view
+
+
 def home(response):
     return render(response, 'analysis/home.html')
 
@@ -24,21 +26,21 @@ def about(response):
 def analysis(request):
     # if this is a POST response we need to process the form data
     if request.method == 'POST':
-        #print(request.POST)
-        #print(request.FILES)
+        # print(request.POST)
+        # print(request.FILES)
         try:
             image = request.FILES.get('image')
             b64_img = base64.b64encode(image.file.read())
             base64_image = str(b64_img)[2:-1]
-            
+
             # default parameters
             name = 'initial'
 
             # prefilling form with base64 image
             data = {
                 # Image
-                'name':name,
-                'base64_image':base64_image,
+                'name': name,
+                'base64_image': base64_image,
                 # Mesh Generation
                 'avg_num_nodes_per_edge': 4,
                 # CellFIT Formulation
@@ -70,7 +72,6 @@ def analysis(request):
             # In this case, the user is updating results
             form = ParametersForm(request.POST)
             data = request.POST
-            
 
         # convert base64 image to PIL Image object
         im = Image.open(BytesIO(base64.b64decode(data['base64_image'])))
@@ -79,29 +80,30 @@ def analysis(request):
         # plot np array using plotly
         import plotly.express as px
         from plotly.offline import plot
-    
+
         fig = px.imshow(img_array)
         config = {
             'scrollZoom': False,
             'displayModeBar': True,
-            'showLink':False,
+            'showLink': False,
             'displaylogo': False
         }
-        plt_div = plot(fig, output_type='div', include_plotlyjs=False, show_link=False, link_text="", config=config)
+        plt_div = plot(fig, output_type='div', include_plotlyjs=False,
+                       show_link=False, link_text="", config=config)
 
         # send all form, information, and plot to html template
         context = {
             'name': data['name'],
             'base64_image': data['base64_image'],
             'plot': plt_div,
-            'form': form 
+            'form': form
         }
         return render(request, 'analysis/output.html', context)
 
     # if a GET (or any other method) we'll create a blank form
     else:
         form = ImageUploadForm()
-        
+
         context = {
             'form': form
         }
